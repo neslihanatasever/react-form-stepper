f464905d69b0066adc652332c04e3150462f98e7
neslihan <neslihan_atasvr111@outlook.com>
2024-02-07 17:57:52 +0300

    field comp and paramters

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import { Formik, Form, Field } from "formik";
+
 function App() {
   return (
-    <div>test</div>
-  )
+    <div>
+      <Formik
+        initialValues={{
+          step: 1,
+          name: "",
+          surname: "",
+          age: "",
+          job: "",
+          about: "",
+        }}
+        onSubmit={(values, actions) => {
+          console.log("values", values);
+        }}
+      >
+        {(values) => (
+          <Form>
+            {values.step === 1 && (
+              <>
+                <Field name="name" />
+                <Field name="surname" />
+              </>
+            )}
+
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
+export default App;
