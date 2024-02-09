b9341e648497824ce320162a145d790341efff8b
neslihan <neslihan_atasvr111@outlook.com>
2024-02-09 04:42:22 +0300

    case'e uyarlamaya calısma

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ function App() {
   return (
     <div>
       <Formik
-      validationSchema={stepperValidation}
+        validationSchema={stepperValidation}
         initialValues={{
           step: 1,
           lastStep: 3,
           name: "",
           surname: "",
+          search: "",
           age: "",
           job: "",
           about: "",
@@ -21,11 +22,11 @@ function App() {
       >
         {({ values, setFieldValue }) => {
           const prevHandle = (e) => {
-            setFieldValue("step", values.step - 1)
+            setFieldValue("step", values.step - 1);
           };
 
           const nextHandle = (e) => {
-            setFieldValue("step", values.step + 1)
+            setFieldValue("step", values.step + 1);
           };
 
           return (
@@ -39,16 +40,51 @@ function App() {
               {values.step == 1 && (
                 <div className="grid  gap-2.5">
                   <div>
-                  <Field name="name" className="input" placeholder="Name" />
-                  <ErrorMessage name="name" component="small" className="block text-xs text-red-600 mt-1" />
+                    <ErrorMessage
+                      name="name"
+                      component="small"
+                      className="block text-xs text-red-600 mt-1"
+                    />
                   </div>
+                  <div className="w-4/5 h-52 flex flex-col ml-40 border-solid border-2 rounded-3xl mt-24">
+                    <div className="m-5 flex flex-col">
+                      <label className="font">Parçanı Seç</label>
+                      <label className="text-lightgray">
+                        Kampanyayı oluşturmak istediğin parçayı seç
+                      </label>
+                    </div>
+
+                    <div className="ml-4">
+                      <Field
+                        name="search"
+                        type="text"
+                        className="input w-[90%] h-10 border-solid border-2 rounded-xl pl-3"
+                        placeholder="Spotify'da Ara..."
+                      />
+                    </div>
+
+                    <div className="flex items-center mt-4 ml-4">
+                      <input
+                        type="checkbox"
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300"
+                      />
+                      <label className="ml-2 mb-1">Parçam Yayında Değil</label>
+                    </div>
+                  </div>
+
+
+                  
                   <div>
-                  <Field
-                    name="surname"
-                    className="input"
-                    placeholder="Surname"
-                  />
-                  <ErrorMessage name="surname" component="small" className="block text-xs text-red-600 mt-1" />
+                    <Field
+                      name="surname"
+                      className="input"
+                      placeholder="Surname"
+                    />
+                    <ErrorMessage
+                      name="surname"
+                      component="small"
+                      className="block text-xs text-red-600 mt-1"
+                    />
                   </div>
                 </div>
               )}
@@ -56,12 +92,20 @@ function App() {
               {values.step == 2 && (
                 <div className="grid  gap-2.5">
                   <div>
-                  <Field name="age" className="input" placeholder="Age" />
-                  <ErrorMessage name="age" component="small" className="block text-xs text-red-600 mt-1" />
+                    <Field name="age" className="input" placeholder="Age" />
+                    <ErrorMessage
+                      name="age"
+                      component="small"
+                      className="block text-xs text-red-600 mt-1"
+                    />
                   </div>
                   <div>
-                  <Field name="job" className="input" placeholder="Job" />
-                  <ErrorMessage name="job" component="small" className="block text-xs text-red-600 mt-1" />
+                    <Field name="job" className="input" placeholder="Job" />
+                    <ErrorMessage
+                      name="job"
+                      component="small"
+                      className="block text-xs text-red-600 mt-1"
+                    />
                   </div>
                 </div>
               )}
@@ -69,13 +113,17 @@ function App() {
               {values.step == 3 && (
                 <div className="grid  gap-2.5">
                   <div>
-                  <Field
-                    name="about"
-                    component="textarea"
-                    className="textarea"
-                    placeholder="About"
-                  />
-                  <ErrorMessage name="about" component="small" className="block text-xs text-red-600 mt-1" />
+                    <Field
+                      name="about"
+                      component="textarea"
+                      className="textarea"
+                      placeholder="About"
+                    />
+                    <ErrorMessage
+                      name="about"
+                      component="small"
+                      className="block text-xs text-red-600 mt-1"
+                    />
                   </div>
                 </div>
               )}
@@ -97,7 +145,7 @@ function App() {
                 ]}
               </div>
             </Form>
-          )
+          );
         }}
       </Formik>
     </div>
